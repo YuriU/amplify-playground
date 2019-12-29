3604f86c9b52291b88645ce6ea019b443fe5910a
YuriU <jerryhire8@gmail.com>
2019-12-29 22:03:30 +0200

    Added authentication buttons

--- 02_Auth/amplify-js-app/src/app.js
+++ 02_Auth/amplify-js-app/src/app.js
@@ -3,11 +3,14 @@ import PubSub from '@aws-amplify/pubsub';
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 import { onCreateTodo } from './graphql/subscriptions'
-
-
+import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
+
 API.configure(awsconfig);
 PubSub.configure(awsconfig);
+//Amplify.configure(awsconfig);
+
+
 
 async function createNewTodo() {
   const todo = { name: "Use AppSync" , description: "Realtime and Offline"}
@@ -27,6 +30,9 @@ const MutationButton = document.getElementById('MutationEventButton');
 const MutationResult = document.getElementById('MutationResult');
 const QueryResult = document.getElementById('QueryResult');
 const SubscriptionResult = document.getElementById('SubscriptionResult');
+const LoginButton = document.getElementById('LoginButton');
+const CheckLoginButton = document.getElementById('CheckLoginButton');
+const LogoutButton = document.getElementById('LogoutButton');
 
 API.graphql(graphqlOperation(onCreateTodo)).subscribe({
     next: (evt) =>{
@@ -44,3 +50,39 @@ MutationButton.addEventListener('click', (evt) => {
   })
 });
 
+CheckLoginButton.addEventListener('click', (evt) => {
+
+  Auth.currentAuthenticatedUser({
+    bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    }).then(user => console.log(user))
+   .catch(err => console.log(err));
+
+
+   Auth.currentSession()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
+
+LogoutButton.addEventListener('click', (evt) => {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+});
+
+
+
+
+// You can get the current config object
+const currentConfig = Auth.configure();
+
+
+LoginButton.addEventListener('click', async (evt) => {
+
+  try {
+    let user = await Auth.federatedSignIn({provider: 'Google'});
+    console.log(user);
+  }
+  catch(err) {
+    console.log(err)
+  }
+})
