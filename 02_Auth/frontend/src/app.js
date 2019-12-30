fbf69d8a065ce51825d996b5df641482a0d6eae8
YuriU <jerryhire8@gmail.com>
2019-12-30 23:13:27 +0200

    Small refactoring & API Call

--- 02_Auth/frontend/src/app.js
+++ 02_Auth/frontend/src/app.js
@@ -4,6 +4,7 @@ import authConfig from './config'
 const LoginButton = document.getElementById('LoginButton');
 const GetUserButton = document.getElementById('buttonGetUser');
 const GetSessionButton = document.getElementById('buttonGetSession');
+const TestEndpointButton = document.getElementById('buttonTestEndpoint');
 const LogoutButton = document.getElementById('LogoutButton');
 const Output = document.getElementById('output');
 
@@ -19,12 +20,11 @@ GetUserButton.addEventListener('click', async (evt) => {
       bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
       });
 
-      Output.innerHTML = JSON.stringify(user, null, 2);
-      console.log(user)
+      OutputWrite(user)
   }
   catch(err) {
     Output.innerHTML = JSON.stringify(err, null, 2);
-    console.log('Error: ' + err)
+    OutputWrite('Error: ' + err)
   }
 });
 
@@ -32,34 +32,55 @@ GetSessionButton.addEventListener('click', async (evt) => {
   try {
     let session = await Auth.currentSession();
     Output.innerHTML = JSON.stringify(session, null, 2);
-    console.log(session)
+    OutputWrite(session)
   }
   catch(err){
-    Output.innerHTML = JSON.stringify(err, null, 2);
-    console.log('Error: ' + err)
+    OutputWrite('Error: ' + err)
   }
 })
 
 LogoutButton.addEventListener('click', async (evt) => {
   try {
     let data = await Auth.signOut()
-    Output.innerHTML = JSON.stringify(data, null, 2);
-    console.log(data)
+    OutputWrite(data)
   }
   catch(err)  {
-    err => console.log(err)
-    console.log('Error: ' + err)
+    OutputWrite('Error: ' + err)
   }
 });
 
 LoginButton.addEventListener('click', async (evt) => {
   try {
     let user = await Auth.federatedSignIn({provider: 'Google'});
-    Output.innerHTML = JSON.stringify(user, null, 2);
-    console.log(user);
+    OutputWrite(user);
   }
   catch(err) {
-    console.log('Error: ' + err)
-    console.log(err)
+    OutputWrite('Error: ' + err)
   }
 })
+
+
+TestEndpointButton.addEventListener('click', async (evt) => {
+  try {
+    let accessToken = await (await Auth.currentSession()).getAccessToken().getJwtToken();
+    
+    let result = await fetch(`https://wdk78z2fl4.execute-api.eu-central-1.amazonaws.com/dev/test`, {
+      method: 'get',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': accessToken
+      })
+    });
+
+    OutputWrite(result)
+
+  }
+  catch(err)  {
+    OutputWrite('Error: ' + err)
+  }
+});
+
+function OutputWrite(value){
+  Output.innerHTML = JSON.stringify(value, null, 2);
+  console.log(value)
+}
