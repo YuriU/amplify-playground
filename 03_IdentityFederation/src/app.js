3bd8b7def2a756bf4d116fcd5f284fffbeb57afe
YuriU <jerryhire8@gmail.com>
2020-01-05 20:19:25 +0200

    Added identity federation demo

---
+++ 03_IdentityFederation/src/app.js
@@ -0,0 +1,77 @@
+import Amplify, { Auth, Storage } from 'aws-amplify';
+import authConfig from './config'
+
+const LoginButton = document.getElementById('LoginButton');
+const GetUserButton = document.getElementById('buttonGetUser');
+const GetSessionButton = document.getElementById('buttonGetSession');
+const GetCredentialsButton = document.getElementById('buttonTestGetCredentials');
+const LogoutButton = document.getElementById('LogoutButton');
+const Output = document.getElementById('output');
+
+console.log(JSON.stringify(authConfig));
+
+Amplify.configure({
+  Auth: authConfig
+});
+  
+GetUserButton.addEventListener('click', async (evt) => {
+  try {
+    let user = await Auth.currentAuthenticatedUser({
+      bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+      });
+
+      OutputWrite(user)
+  }
+  catch(err) {
+    Output.innerHTML = JSON.stringify(err, null, 2);
+    OutputWrite('Error: ' + err)
+  }
+});
+
+GetSessionButton.addEventListener('click', async (evt) => {
+  try {
+    let session = await Auth.currentSession();
+    Output.innerHTML = JSON.stringify(session, null, 2);
+    OutputWrite(session)
+  }
+  catch(err){
+    OutputWrite('Error: ' + err)
+  }
+})
+
+LogoutButton.addEventListener('click', async (evt) => {
+  try {
+    let data = await Auth.signOut()
+    OutputWrite(data)
+  }
+  catch(err)  {
+    OutputWrite('Error: ' + err)
+  }
+});
+
+LoginButton.addEventListener('click', async (evt) => {
+  try {
+    let user = await Auth.federatedSignIn({provider: 'Google'});
+    OutputWrite(user);
+  }
+  catch(err) {
+    OutputWrite('Error: ' + err)
+  }
+})
+
+
+GetCredentialsButton.addEventListener('click', async (evt) => {
+  try {
+    let credentials = await Auth.currentCredentials();
+    OutputWrite(credentials.data.Credentials);
+  }
+  catch(err) {
+    OutputWrite('Error: ' + err)
+  }
+});
+
+function OutputWrite(value){
+  Output.innerHTML = JSON.stringify(value, null, 2);
+  console.log('>>> ' + value)
+  console.log(value)
+}
